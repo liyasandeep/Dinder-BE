85f924b4187e440a45b1214a0c06e2d231cec9a1
liyasandeep <liyasandeep1@gmail.com>
2022-11-21 19:39:10 +0000

    listen port change

--- listen.js
+++ listen.js
@@ -1,13 +1,12 @@
 const app = require("./app");
-const PORT = process.env.PORT || 3000
-const connectDB = require("./database/connection")
-
+const PORT = process.env.PORT || 3000;
+const connectDB = require("./database/connection");
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-      console.log("listening for requests");
-  })
-})
+    console.log(`listening for requests on ${PORT}`);
+  });
+});
 // app.listen(PORT, () => {
 //   console.log(`Listening on ${PORT}...`);
 // });
